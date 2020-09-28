bb8b9fc3d6a10f3b6478de05b7725825d41fdc68
H junior <13500154+kjuniors2@users.noreply.github.com>
2020-09-27 22:36:37 -0300

    Update flake8 test, add conftest

--- libpythonpro/tests/test_spam/test_enviador_de_email.py
+++ libpythonpro/tests/test_spam/test_enviador_de_email.py
@@ -7,6 +7,7 @@ def test_criar_enviador_de_email():
     enviador = Enviador()
     assert enviador is not None
 
+
 @pytest.mark.parametrize(
     'remetente',
     ['foo@bar', 'renzo@python.pro.br']
