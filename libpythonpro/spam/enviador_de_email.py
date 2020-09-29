08d49e58f23454487e196c13d714e70faa31a6db
H junior <13500154+kjuniors2@users.noreply.github.com>
2020-09-28 22:31:13 -0300

    Nesse tópico você vai aprender o conceito de injeção de dependências para poder produzir teste unitários

--- libpythonpro/spam/enviador_de_email.py
+++ libpythonpro/spam/enviador_de_email.py
@@ -1,11 +1,7 @@
 class Enviador:
-    def __init__(self):
-        self.qt_email_enviados = 0
-
     def enviar(self, remetente, destinatario, assunto, corpo):
         if '@' not in remetente:
             raise EmailInvalido(f'Email de remetente inválido: {remetente}')
-        self.qt_email_enviados += 1
         return remetente
 
 
--- libpythonpro/tests/test_spam/test_envio_para_base_de_usuarios.py
+++ libpythonpro/tests/test_spam/test_envio_para_base_de_usuarios.py
@@ -21,7 +21,7 @@ from libpythonpro.spam.modelos import Usuario
 def test_qde_de_spam(sessao, usuarios):
     for usuario in usuarios:
         sessao.salvar(usuario)
-    enviador = Enviador()
+    enviador = EnviadorMock()
     enviador_de_spam = EnviadorDeSpam(sessao, enviador)
     enviador_de_spam.enviar_emails(
         'junior@foo.bar',
@@ -29,3 +29,31 @@ def test_qde_de_spam(sessao, usuarios):
         'Confira os modulos saso fantasticos'
     )
     assert len(usuarios) == enviador.qt_email_enviados
+
+class EnviadorMock(Enviador):
+    def __init__(self):
+        super().__init__()
+        self.parametros_de_envio = None
+        self.qt_email_enviados = 0
+
+    def enviar(self, remetente, destinatario, assunto, corpo):
+        self.paramentros_de_envio = (remetente, destinatario, assunto, corpo)
+        self.qt_email_enviados += 1
+
+
+def test_parametros_de_spam(sessao):
+    usuario = Usuario(nome='Junior', email='junior@foo.bar')
+    sessao.salvar(usuario)
+    enviador = EnviadorMock()
+    enviador_de_spam = EnviadorDeSpam(sessao, enviador)
+    enviador_de_spam.enviar_emails(
+        'junior@foo.bar',
+        'Curso de Python',
+        'Confira os modulos saso fantasticos'
+    )
+    assert enviador.paramentros_de_envio == (
+        'junior@foo.bar',
+        'junior@foo.bar',
+        'Curso de Python',
+        'Confira os modulos saso fantasticos',
+    )
