4b09f744b7d5bd9704fdbaa2285d18919503671d
H junior <13500154+kjuniors2@users.noreply.github.com>
2020-10-12 17:37:09 -0300

    Nesse tópico você vai perceber que ao manipular imports o isolamento de testes pode ficar comprometido.

--- libpythonpro/tests/test_spam/test_github_api.py
+++ libpythonpro/tests/test_spam/test_github_api.py
@@ -1,14 +1,29 @@
 from unittest.mock import Mock
 
+import pytest
+
 from libpythonpro import github_api
 
 
-def test_buscar_avatar():
+@pytest.fixture
+def avatar_url():
     resp_mock = Mock()
+    url = 'https://avatars1.githubusercontent.com/u/XXXXXXXXXXXX=4'
     resp_mock.json.return_value = {
          'login': 'user_name', 'id': 13154, 'node_id': 'M5777777777777zNTAwMTU0',
-         'avatar_url': 'https://avatars1.githubusercontent.com/u/XXXXXXXXXXXX=4',
+         'avatar_url': url
     }
+    resp_original = github_api.requests.get
     github_api.requests.get = Mock(return_value=resp_mock)
-    url = github_api.buscar_avatar('user_name')
-    assert 'https://avatars1.githubusercontent.com/u/XXXXXXXXXXXX=4' == url
+    yield url
+    github_api.requests.get = resp_original
+
+
+def test_buscar_avatar(avatar_url):
+    url = github_api.buscar_avatar('renzo')
+    assert avatar_url == url
+
+
+def test_buscar_avatar_url():
+    url = github_api.buscar_avatar('renzon')
+    assert 'https://avatars3.githubusercontent.com/u/3457115?v=4' == url
